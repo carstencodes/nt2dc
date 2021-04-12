c271704f5db1cf995d49aea0ad655a6e88beb273
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-04-12 22:07:30 +0200

    "Bumped version from 0.8.0 to 0.9.0"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ copyright = "2021, Carsten Igel"
 author = "Carsten Igel"
 
 # The full version, including alpha/beta/rc tags
-release = "0.8.0"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
